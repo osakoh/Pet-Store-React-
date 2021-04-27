9155b4dbf0081ae9bb0e1da5f9a481ae567fa9ea
osakoh <macmive@gmail.com>
2021-04-28 00:13:07 +0100

    created key prop for listAppointments

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,55 @@
+import React, { Component } from 'react';
 import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
+import axios from 'axios'; // axios import
 
 
-function App() {
-  return (
+class App extends Component {
 
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+  state = {
+    myAppointments: [],
+    lastIndex: 0,
+  }
+
+  // lifecycle method
+  async componentDidMount() {
+    const response = await axios.get('./data.json');
+
+    const apts = response.data.map(item => {
+      item.aptId = this.state.lastIndex;
+      this.setState({ lastIndex: this.state.lastIndex + 1 }); // modifies the index value on each iteration
+      return item;
+    });
+
+    // reset the states
+    this.setState({ myAppointments: apts });
+
+  }
+
+  render() {
+    // destructuring myAppointments from state
+    const { myAppointments } = this.state;
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={myAppointments} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
 
-  );
+    );
+  }
+
 }
 
 export default App;
@@ -36,4 +64,5 @@ export default App;
  *
  * racf: functional component
  * rce: class based component that exports at the bottom
+ * rafce -react arrow function export Component
 */
