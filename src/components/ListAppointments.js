58490e8ecf9bed7535799617cf0455b7d24bb8ad
osakoh <macmive@gmail.com>
2021-04-28 00:40:09 +0100

    used Moment to format the appointment date

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaTimes } from "react-icons/fa";
+import Moment from 'react-moment';
 
 class ListAppointments extends Component {
     render() {
@@ -17,14 +18,20 @@ class ListAppointments extends Component {
                         <div className="pet-info media-body">
                             <div className="pet-head d-flex">
                                 <span className="pet-name">{item.petName}</span>
-                                <span className="apt-date ml-auto">{item.aptDate}</span>
+                                <span className="apt-date ml-auto">
+                                    <Moment
+                                        date={item.aptDate}
+                                        parse="YYYY-MM-dd hh:mm"
+                                        format="DD/MM/YYYY h:mA"
+                                    />
+                                </span>
                             </div>
 
                             <div className="owner-name">
                                 <span className="label-item">Owner: </span>
-                                <span>ownerName</span>
+                                <span>{item.ownerName}</span>
                             </div>
-                            <div className="apt-notes">aptNotes</div>
+                            <div className="apt-notes">{item.aptNotes}</div>
                         </div>
                     </div>
                 ))}
